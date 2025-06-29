7e8f65de9277859004901e50e636ece7a2f42db7
ryuone <ryuone@gmail.com>
2025-06-29 11:52:35 +0900

    Update FEED_INFO_LIST with additional feeds.

--- src/resources/feed-info-list.ts
+++ src/resources/feed-info-list.ts
@@ -27,12 +27,22 @@ const createFeedInfoList = (feedInfoTuples: FeedInfoTuple[]) => {
 // prettier-ignore
 export const FEED_INFO_LIST: FeedInfo[] = createFeedInfoList([
   // ['企業名・製品名など', 'RSS/AtomフィードのURL'],
-  ['https://2ality.com', 'https://2ality.com/feeds/posts.atom'],
+  ['2ality – JavaScript and more', 'https://2ality.com/feeds/posts.atom'],
+  ['Stories by Kazuki Kyakuno on Medium', 'https://medium.com/feed/@kyakuno'],
+
+  // 企業のBlog
   ['OpenAI News', 'https://openai.com/news/rss.xml'],
   ['TECH BLOG | 株式会社AI Shift', 'https://www.ai-shift.co.jp/techblog/feed'],
+  ['Platinum Data Blog by BrainPad ブレインパッド', 'https://blog.brainpad.co.jp/rss'],
+  ['PublicKey', 'https://www.publickey1.jp/atom.xml'],
+  ['メルカリエンジニアリングブログ', 'https://engineering.mercari.com/blog/feed.xml'],
+  ['リクルートTech Blog', 'https://blog.recruit.co.jp/data/index.xml'],
+  ['LINEヤフーTech Blog', 'https://techblog.lycorp.co.jp/ja/feed/index.xml'],
+  ['DeNA Engineering on DeNA Engineering', 'https://engineering.dena.com/index.xml'],
+  ['NTT Communications Engineers\' Blog', 'https://engineers.ntt.com/feed'],
+
+  // Zenn, Qiita, Noteが提供しているfeed
   ['Zennの「LLM」のフィード', 'https://zenn.dev/topics/llm/feed'],
   ['LLMタグが付けられた新着記事 - Qiita', 'https://qiita.com/tags/llm/feed'],
-  ['#LLMタグ', 'https://note.com/hashtag/LLM/rss'],
-  ['Platinum Data Blog by BrainPad ブレインパッド', 'https://blog.brainpad.co.jp/rss'],
-  ['Stories by Kazuki Kyakuno on Medium', 'https://medium.com/feed/@kyakuno'],
+  ['「#LLM」の人気タグ記事一覧｜note ――つくる、つながる、とどける。', 'https://note.com/hashtag/LLM/rss'],
 ]);
