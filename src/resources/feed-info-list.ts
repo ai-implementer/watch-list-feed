2bd1ee23cd63c8032ffae867f3f04985a86805f0
ryuone <ryuone@gmail.com>
2025-07-19 21:41:28 +0900

    Update FEED_INFO_LIST with additional blog and developer feeds, and remove one obsolete entry

--- src/resources/feed-info-list.ts
+++ src/resources/feed-info-list.ts
@@ -29,6 +29,11 @@ export const FEED_INFO_LIST: FeedInfo[] = createFeedInfoList([
   // ['企業名・製品名など', 'RSS/AtomフィードのURL'],
   ['2ality – JavaScript and more', 'https://2ality.com/feeds/posts.atom'],
   ['Stories by Kazuki Kyakuno on Medium', 'https://medium.com/feed/@kyakuno'],
+  ['Mitsuyuki.Shiiba', 'https://bufferings.hatenablog.com/rss'],
+  ['橋本商会', 'https://scrapbox.io/api/feed/shokai'],
+  ['yhara.jp', 'https://yhara.jp/_feed.xml'],
+  ['きしだのはてな', 'https://nowokay.hatenablog.com/feed'],
+  ['WWW WATCH', 'https://feeds.feedburner.com/wacth'],
 
   // 企業のBlog
   ['OpenAI News', 'https://openai.com/news/rss.xml'],
@@ -38,7 +43,6 @@ export const FEED_INFO_LIST: FeedInfo[] = createFeedInfoList([
   ['メルカリエンジニアリングブログ', 'https://engineering.mercari.com/blog/feed.xml'],
   ['リクルートTech Blog', 'https://blog.recruit.co.jp/data/index.xml'],
   ['LINEヤフーTech Blog', 'https://techblog.lycorp.co.jp/ja/feed/index.xml'],
-  ['DeNA Engineering on DeNA Engineering', 'https://engineering.dena.com/index.xml'],
   ["NTT Communications Engineers' Blog", 'https://engineers.ntt.com/feed'],
   ['Google Research Blog', 'https://research.google/blog/rss/'],
   ['カミナシ エンジニアブログ', 'https://kaminashi-developer.hatenablog.jp/feed'],
@@ -50,6 +54,8 @@ export const FEED_INFO_LIST: FeedInfo[] = createFeedInfoList([
   ['ログラス (Zenn)', 'https://zenn.dev/p/loglass/feed'],
   ['一休.com Developers Blog', 'https://user-first.ikyu.co.jp/feed'],
   ['タイミー', 'https://tech.timee.co.jp/feed'],
+  ['O\'Reilly Radar - Insight, analysis, and research about emerging technologies', 'https://feeds.feedburner.com/oreilly/radar/atom'],
+  ['Developers.IO', 'https://dev.classmethod.jp/feed/'],
 
   // Zenn, Qiita, Noteが提供しているfeed
   ['Zennの「LLM」のフィード', 'https://zenn.dev/topics/llm/feed'],
