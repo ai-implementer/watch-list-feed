b7dfff94859b5e0918b14aa76a68eef113646dde
ryuone <ryuone@gmail.com>
2026-02-23 22:33:05 +0900

    Update `feed-info-list.ts` with additional blog and tech media feed links; reorganize categories.

--- src/resources/feed-info-list.ts
+++ src/resources/feed-info-list.ts
@@ -27,20 +27,23 @@ const createFeedInfoList = (feedInfoTuples: FeedInfoTuple[]) => {
 // prettier-ignore
 export const FEED_INFO_LIST: FeedInfo[] = createFeedInfoList([
   // ['企業名・製品名など', 'RSS/AtomフィードのURL'],
+
+  // 個人ブログ
   ['2ality – JavaScript and more', 'https://2ality.com/feeds/posts.atom'],
   ['Stories by Kazuki Kyakuno on Medium', 'https://medium.com/feed/@kyakuno'],
   ['Mitsuyuki.Shiiba', 'https://bufferings.hatenablog.com/rss'],
   ['橋本商会', 'https://scrapbox.io/api/feed/shokai'],
   ['yhara.jp', 'https://yhara.jp/_feed.xml'],
   ['きしだのはてな', 'https://nowokay.hatenablog.com/feed'],
-  ['WWW WATCH', 'https://feeds.feedburner.com/wacth'],
-  ['PYMNTS.com', 'https://www.pymnts.com/feed/'],
+  ["Simon Willison's Weblog", 'https://simonwillison.net/atom/everything/'],
+  ['hang-up | note', 'https://note.com/git_yamazaki/rss'],
+  ['akira_cloudjob | Zenn', 'https://zenn.dev/akira_cloudjob/feed'],
+  ['TooMe | Qiita', 'https://qiita.com/TooMe/feed'],
 
-  // 企業のBlog
+  // 企業テックブログ
   ['OpenAI News', 'https://openai.com/news/rss.xml'],
   ['TECH BLOG | 株式会社AI Shift', 'https://www.ai-shift.co.jp/techblog/feed'],
   ['Platinum Data Blog by BrainPad ブレインパッド', 'https://blog.brainpad.co.jp/rss'],
-  ['PublicKey', 'https://www.publickey1.jp/atom.xml'],
   ['メルカリエンジニアリングブログ', 'https://engineering.mercari.com/blog/feed.xml'],
   ['リクルートTech Blog', 'https://blog.recruit.co.jp/data/index.xml'],
   ['LINEヤフーTech Blog', 'https://techblog.lycorp.co.jp/ja/feed/index.xml'],
@@ -55,17 +58,18 @@ export const FEED_INFO_LIST: FeedInfo[] = createFeedInfoList([
   ['ログラス (Zenn)', 'https://zenn.dev/p/loglass/feed'],
   ['一休.com Developers Blog', 'https://user-first.ikyu.co.jp/feed'],
   ['タイミー', 'https://tech.timee.co.jp/feed'],
-  ["O'Reilly Radar", 'https://feeds.feedburner.com/oreilly/radar/atom'],
-  ['Developers.IO', 'https://dev.classmethod.jp/feed/'],
   ['Socket', 'https://socket.dev/api/blog/feed.atom'],
   ['Cybozu Inside Out', 'https://blog.cybozu.io/rss'],
 
-  // Zenn, Qiita, Noteが提供しているfeed
+  // テックメディア
+  ['WWW WATCH', 'https://feeds.feedburner.com/wacth'],
+  ['PYMNTS.com', 'https://www.pymnts.com/feed/'],
+  ['PublicKey', 'https://www.publickey1.jp/atom.xml'],
+  ["O'Reilly Radar", 'https://feeds.feedburner.com/oreilly/radar/atom'],
+  ['Developers.IO', 'https://dev.classmethod.jp/feed/'],
+
+  // トピックフィード
   ['Zennの「LLM」のフィード', 'https://zenn.dev/topics/llm/feed'],
   ['LLMタグが付けられた新着記事 - Qiita', 'https://qiita.com/tags/llm/feed'],
   // ['「#LLM」の人気タグ記事一覧｜note ――つくる、つながる、とどける。', 'https://note.com/hashtag/LLM/rss'],
-  ["Simon Willison's Weblog", 'https://simonwillison.net/atom/everything/'],
-  ['hang-up | note', 'https://note.com/git_yamazaki/rss'],
-  ['akira_cloudjob | Zenn', 'https://zenn.dev/akira_cloudjob/feed'],
-  ['TooMe | Qiita', 'https://qiita.com/TooMe/feed'],
 ]);
